5779c739b5898ac06f8f7c6d522159ced4b8fecd
Sumit kumar mahto <110152045+codersumit01@users.noreply.github.com>
2022-11-13 18:56:09 +0530

    Update App.js
    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,10 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+ReactDOM.render(<p>Hello</p>, document.getElementById('root'));
+                
+                export default App;
