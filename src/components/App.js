88441297069091db788c6b105e340cd5439d1366
Sumit kumar mahto <110152045+codersumit01@users.noreply.github.com>
2022-11-16 11:56:50 +0530

    Update App.js
    cimmit changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-// <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 
 function App() {
     return (
-       
+       <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     )
 }
-ReactDOM.render(<p>Hello</p>, document.getElementById('root'));
+
                 
   export default App;
